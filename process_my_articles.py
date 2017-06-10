eaf4e341293629fa67201a023d48db45322e47fc
Full Bright <full3right@gmail.com>
2017-06-10 16:18:06 +0200

    Pull articles from getpocket

--- process_my_articles.py
+++ process_my_articles.py
@@ -1,5 +1,6 @@
 from pocket import Pocket, PocketException
 import json
+import os
 
 j = json.loads(open("config.json").read())
 
@@ -9,8 +10,44 @@ p = Pocket(
 )
 
 # Fetch a list of articles
+
+mycount = 100
+myoffset = 0
+tagfilter = "garyreporter"
+articles_json_data_file = 'articles_json_data.json'
+
 try:
-    print(p.retrieve(offset=0, count=10))
+    #articles_json = json.loads(p.retrieve(offset=myoffset, count=mycount))
+    articles_json = None
+
+    if not os.path.isfile(articles_json_data_file):
+        print "Requesting articles from GetPocket ..."
+        articles_json = p.retrieve(tag=tagfilter, offset=myoffset, count=mycount)
+
+        with open(articles_json_data_file, 'w') as outfile:
+            json.dump(articles_json, outfile)
+
+
+    else:
+        print "Reading local articles from file"
+        articles_json = json.loads(open(articles_json_data_file).read())
+
+    print("Status of request : %s. Got %s articles" % (articles_json['status'], len(articles_json['list'])))
+
+    for article in articles_json['list']:
+        print "--- --- ---"
+        art_title =  articles_json['list'][article]['resolved_title'].encode('utf-8')
+        art_url = articles_json['list'][article]['resolved_url'].encode('utf-8')
+        art_id = articles_json['list'][article]['resolved_id'].encode('utf-8')
+
+        print "Found new article : >>>>> #%s - (%s)[%s]" % (art_id, art_title, art_url)
+        # If processed successfully, remove the tag from it
+        p.tags_remove(art_id, tagfilter)
+
+
+    # Finally remove the articles json file
+    os.remove(articles_json_data_file)
+
 except PocketException as e:
     print(e.message)
 
