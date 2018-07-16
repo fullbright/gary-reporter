67752442760221c2e53990bb5dd10f1e045d74a1
Full Bright <full3right@gmail.com>
2018-07-16 07:48:08 +0200

    Add lastest updates to script

--- nltk_training/information_extraction.py
+++ nltk_training/information_extraction.py
@@ -10,13 +10,19 @@ from nltk import word_tokenize
 import urllib2
 
 def ie_preprocess(document):
-	sentences = nltk.sent_tokenize(document) [1]
-	sentences = [nltk.word_tokenize(sent) for sent in sentences] [2]
+	sentences = nltk.sent_tokenize(document)
+	sentences = [nltk.word_tokenize(sent) for sent in sentences]
 	sentences = [nltk.pos_tag(sent) for sent in sentences]
 
 	return sentences
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-with open(dir_path+"/../bible_fulltext/Bible_French_djvu.txt") as file:
-	ie_preprocess(file.read().decode('utf8'))
+with open(dir_path+"/../bible_fulltext/Bible_French_djvu_Genesis.txt") as file:
+	sentences = ie_preprocess(file.read().decode('utf8'))
 
+	cp = nltk.RegexpParser('CHUNK: {<V.*> <TO> <V.*>}')
+	for sent in sentences:
+		#print sent
+		tree = cp.parse(sent)
+		for subtree in tree.subtrees():
+			if subtree.label() == 'CHUNK': print (subtree)
